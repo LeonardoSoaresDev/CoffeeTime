65650aa647e3a28b754a93bd08a999075642fe2f
Leonardo <leo_s_arauujo@hotmail.com>
2021-01-18 15:51:40 -0300

    Find a coffee specified by the name and shows it in json :construction:

--- src/main/java/com/coffeetime/coffee/repositorys/coffeeRepository/CoffeeRepository.java
+++ src/main/java/com/coffeetime/coffee/repositorys/coffeeRepository/CoffeeRepository.java
@@ -4,4 +4,5 @@ import com.coffeetime.coffee.models.coffeeModels.CoffeeModel;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CoffeeRepository extends CrudRepository<CoffeeModel,Long> {
+    CoffeeModel findByCoffeeName(String coffeeName);
 }
